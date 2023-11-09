fb7a198bd57a4952723ec973ea0c1fa38fa310ee
carloshoras <carloshoras>
2023-11-09 19:27:48 +0100

    entregable 2 hecho

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
  // Aquí tu código
+const agregar = document.getElementById('agregar');
+const ulLista = document.getElementById('lista');
+agregar.addEventListener('click', function () {
+    const texto = prompt('Qué elemento quieres introducir?');
+    const ilElement = document.createElement('li');
+    ilElement.textContent = texto;
+    ulLista.appendChild(ilElement);
+})
